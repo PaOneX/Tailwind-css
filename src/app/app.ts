e8588cb7e5202be1260d07e6399cff667d176d21
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-02-17 15:17:31 +0530

    Refactor code structure for improved readability and maintainability

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite/lib/esm/components';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,11 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('Tailwind-css');
+
+   ngOnInit(): void {
+    initFlowbite();
+  }
+
 }
